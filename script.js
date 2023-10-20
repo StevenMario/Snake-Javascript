b7c42f71327c677096d20d208b850ebbfec491ae
StevenMario <stevenkoumar@gmail.com>
2023-10-20 12:23:30 +0300

    Mis à  jour du code

--- script.js
+++ script.js
@@ -1,41 +1,71 @@
 const canvas = document.querySelector("canvas"); // Sélectionne le canvas dans le HTML
 const ctx = canvas.getContext('2d'); // Obtient le contexte de rendu 2D pour le canvas
+const startButton = document.getElementById("start-button");
 let box = 20; // La taille d'une unité de la grille du jeu
 let snake = [];
+
 snake[0] = { x: 10 * box, y: 10 * box }; // Position initiale de la tête du serpent
+
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box, // Position initiale de la nourriture (aléatoire)
     y: Math.floor(Math.random() * 15 + 1) * box
 };
 
+let gameStarted = false; // Variable pour indiquer si le jeu a commencé
 let score = 0; // Score initial
 let d; // Direction du serpent (LEFT, UP, RIGHT, DOWN)
+let snakeVisible = false;
+let elementVisible = false;
+
 
 document.addEventListener("keydown", direction); // Écoute les événements de touche
+// Gestionnaire d'événement pour le bouton "Start"
+startButton.addEventListener("click", startGame);
 
-function direction(event) {
-    let key = event.keyCode;
-    if (key == 37 && d != "RIGHT") {
-        d = "LEFT";
-    } else if (key == 38 && d != "DOWN") {
-        d = "UP";
-    } else if (key == 39 && d != "LEFT") {
-        d = "RIGHT";
-    } else if (key == 40 && d != "UP") {
-        d = "DOWN";
+function startGame() {
+    if (!gameStarted) {
+        startButton.style.display = "none"; // Masque le bouton "Start"
+        snakeVisible = true;
+        elementVisible = true;
+        gameStarted = true; // Marque le jeu comme ayant commencé
     }
 }
 
-function draw() {
-    ctx.clearRect(0, 0, 400, 400); // Efface le canvas à chaque mise à jour
-    for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = (i == 0) ? "green" : "white"; // Couleur de la tête en vert, du corps en blanc
-        ctx.fillRect(snake[i].x, snake[i].y, box, box);
-        ctx.strokeStyle = "red";
-        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+function direction(event) {
+    if(gameStarted){
+        let key = event.keyCode;
+        if (key == 37 && d != "RIGHT") {
+            d = "LEFT";
+        } else if (key == 38 && d != "DOWN") {
+            d = "UP";
+        } else if (key == 39 && d != "LEFT") {
+            d = "RIGHT";
+        } else if (key == 40 && d != "UP") {
+            d = "DOWN";
+        }
     }
-    ctx.fillStyle = "orange";
-    ctx.fillRect(food.x, food.y, box, box); // Dessine la nourriture
+   
+}
+
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Efface le canvas à chaque mise à jour
+    if (snakeVisible) {
+        for (let i = 0; i < snake.length; i++) {
+            ctx.fillStyle = (i == 0) ? "green" : "white"; // Couleur de la tête en vert, du corps en blanc
+            ctx.fillRect(snake[i].x, snake[i].y, box, box);
+            ctx.strokeStyle = "red";
+            ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+        }
+    }
+    if(elementVisible){
+        ctx.fillStyle = "orange";
+        ctx.fillRect(food.x, food.y, box, box); // Dessine la nourriture
+        ctx.fillStyle = "red";
+        ctx.font = "30px Arial";
+        ctx.fillText(score, 2 * box, 1.6 * box); // Affiche le score
+    }
+
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -63,10 +93,9 @@ function draw() {
         clearInterval(game); // Arrête le jeu en cas de collision avec le mur ou soi-même
     }
     snake.unshift(newHead); // Ajoute la nouvelle tête du serpent
-    ctx.fillStyle = "red";
-    ctx.font = "30px Arial";
-    ctx.fillText(score, 2 * box, 1.6 * box); // Affiche le score
 }
+
+
 
 function collision(head, array) {
     for (let j = 0; j < array.length; j++) {
